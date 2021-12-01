9ddd376d229492dcbef862af7f5dab66ce6cb9a4
Fabiano Luiz <fabianolvs3@gmail.com>
2021-11-30 21:31:08 -0300

    dome anotations

--- slicing/substring.d
+++ slicing/substring.d
@@ -17,3 +17,12 @@ void main(){
     writefln("UTF-32 Code unit at index %d is %c", c, c);
   }
 }
+
+// D also supports UTF-16 and UTF-32 strings. These are typed wstring and dstring, 
+// respectively. Let's look at each of these as follows:
+//  -> wstring: This is very useful on Windows, because the Windows operating system 
+//     natively works with UTF-16.
+//
+//  -> dstring: This eats a lot of memory, about 4 times more than strings for English text, 
+//     but sidesteps some of the issues discussed here. The reason is that each array index 
+//     corresponds to one Unicode code point
